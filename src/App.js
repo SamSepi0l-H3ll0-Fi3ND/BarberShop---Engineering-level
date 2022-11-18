2bcb47af852bbfe82f6e8987f121494ea27ef2cd
SamSepi0l-H3ll0-Fi3ND <kubafigzal@gmail.com>
2022-11-18 17:23:26 +0100

    Making order in project (Create correct folders & making it clear)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 import "./App.css";
-import About from "./Pages/About";
-import Hero from "./Pages/Hero";
-import Motto from "./Pages/Motto";
-import Services from "./Pages/Services";
-import Rewiews from "./Pages/Rewiews";
-import Reservation from "./Pages/Reservation";
-import Footer from "./Components/Footer";
+import HeroPage from "./Pages/HeroPage";
 function App() {
   return (
     <div className="App">
-      <Hero></Hero>
-      <Services></Services>
-      <Motto></Motto>
-      <About></About>
-      <Rewiews></Rewiews>
-      <Reservation></Reservation>
-      <Footer></Footer>
+      <HeroPage></HeroPage>
     </div>
   );
 }
---
+++ src/Pages/HeroPage.js
@@ -0,0 +1,23 @@
+import Hero from "../Subpages/Hero";
+import Services from "../Subpages/Services";
+import Motto from "../Subpages/Motto";
+import About from "../Subpages/About";
+import Rewiews from "../Subpages/Rewiews";
+import Reservation from "../Subpages/Reservation";
+import Footer from "../Components/Footer";
+
+const HeroPage = () => {
+  return (
+    <div>
+      <Hero></Hero>
+      <Services></Services>
+      <Motto></Motto>
+      <About></About>
+      <Rewiews></Rewiews>
+      <Reservation></Reservation>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default HeroPage;
